9bb20609e9f2ab48b691f64275a853d952fe6d5d
trunglam2002 <mastertryn123@gmail.com>
2024-03-28 02:37:49 +0700

    stetup_change

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical-chatbot',
+    version='0.0.0',
+    author='Lam Nguyen',
+    author_email='nguyentrunglam2002@gmail.com',
+    packages=find_packages(),
+    instal_requires=[],
+)
